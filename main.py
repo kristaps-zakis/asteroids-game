59209a55c44634019214999683c6945b92288457
Kristaps Zaķis <kristaps.zakis@gmail.com>
2024-12-06 12:03:21 +0200

    removed prints, added pygame.init

--- main.py
+++ main.py
@@ -6,19 +6,15 @@ import pygame
 from constants import *
 
 def main():
-    print ("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
-        print("Running...")
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill((0, 0, 0))
+        screen.fill(black)
         pygame.display.flip()
 
 if __name__ == "__main__":
